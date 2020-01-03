185a395ca7e51e566990af8b74045746d53320ad
Tomasz Kartasiński <tomasz.kartasinski@cybercom.com>
2020-01-03 08:15:44 +0100

    Updated RestApi

--- src/main/java/com/tkjavadev/adventuregame/controllers/GameRestController.java
+++ src/main/java/com/tkjavadev/adventuregame/controllers/GameRestController.java
@@ -5,17 +5,9 @@ import com.tkjavadev.adventuregame.domain.Gate;
 import com.tkjavadev.adventuregame.domain.Item;
 import com.tkjavadev.adventuregame.domain.Location;
 import com.tkjavadev.adventuregame.services.GameService;
-import com.tkjavadev.adventuregame.util.AttributeNames;
-import com.tkjavadev.adventuregame.util.GameMappings;
-import com.tkjavadev.adventuregame.util.ViewNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,31 +19,28 @@ import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/game")
 public class GameRestController {
-    private final Logger log= LoggerFactory.getLogger(GameController.class);
+    private final Logger log = LoggerFactory.getLogger(GameController.class);
 
     // == fields ==
     private GameService gameService;
 
-    private WebDataBinder webDataBinder;
-
     private synchronized void loadInitVariables(WebSession webSession) {
-        if(!webSession.getAttributes().isEmpty()) {
+        if (!webSession.getAttributes().isEmpty()) {
             gameService.setInitVariables((InitVariables) webSession.getAttributes().get(webSession.getId()));
-            log.info("loaded variables for = {}",webSession.getId());
+            log.info("loaded variables for = {}", webSession.getId());
         } else {
             this.gameService.reset();
             webSession.getAttributes().put(webSession.getId(), gameService.getInitVariables());
-            log.info("new variables for = {}",webSession.getId());
+            log.info("new variables for = {}", webSession.getId());
         }
     }
 
-    private synchronized void replace(WebSession webSession){
-        webSession.getAttributes().replace(webSession.getId(),gameService.getInitVariables());
+    private synchronized void replace(WebSession webSession) {
+        webSession.getAttributes().replace(webSession.getId(), gameService.getInitVariables());
     }
 
     // == constructors ==
@@ -60,66 +49,62 @@ public class GameRestController {
     }
 
     // == methods ==
-    @InitBinder
-    public void initBinder(WebDataBinder webDataBinder){
-        this.webDataBinder=webDataBinder;
-    }
-
     @GetMapping("/gates")
-    public @ResponseBody Flux<Gate> getGates(WebSession webSession) {
+    public @ResponseBody
+    Flux<Gate> getGates(WebSession webSession) {
         loadInitVariables(webSession);
         replace(webSession);
         return gameService.getAvailableGates();
     }
 
     @GetMapping("/items")
-    public @ResponseBody Flux<Item> getItems(WebSession webSession) {
+    public @ResponseBody
+    Flux<Item> getItems(WebSession webSession) {
         loadInitVariables(webSession);
         replace(webSession);
         return gameService.getAvailableItems();
     }
 
     @GetMapping("/location")
-    public @ResponseBody Mono<Location> getLocation(WebSession webSession) {
+    public @ResponseBody
+    Mono<Location> getLocation(WebSession webSession) {
         loadInitVariables(webSession);
         replace(webSession);
         return gameService.getLocation();
     }
 
     @GetMapping("/inventory")
-    public @ResponseBody List<Item> getInventory(WebSession webSession) {
+    public @ResponseBody
+    List<Item> getInventory(WebSession webSession) {
         loadInitVariables(webSession);
         replace(webSession);
-        List<Item> inventoryItems=new ArrayList<>();
-        for(String item:gameService.getInventory()){
+        List<Item> inventoryItems = new ArrayList<>();
+        for (String item : gameService.getInventory()) {
             Item invItem = new Item();
             invItem.setName(item);
             inventoryItems.add(invItem);
         }
         return inventoryItems;
     }
-//
-//    @GetMapping("/results")
-//    public @ResponseBody Mono<Location> getLocation(WebSession webSession) {
-//        loadInitVariables(webSession);
-//        replace(webSession);
-//        return gameService.;
-//    }
+
+    @GetMapping("/results")
+    public @ResponseBody List<String> getResults(WebSession webSession) {
+        loadInitVariables(webSession);
+        replace(webSession);
+        List<String> results = new ArrayList<>();
+        results.add(gameService.getVisitedLocations().toString());
+        results.add(gameService.getScore().toString());
+        results.add(gameService.getRank());
+        return results;
+    }
 
 
     @PostMapping("/change")
     public void changeDirection(@RequestBody Gate gate, WebSession webSession) {
         loadInitVariables(webSession);
-//        webDataBinder.validate();
-//
-//        BindingResult bindingResult=webDataBinder.getBindingResult();
-//        if(bindingResult.hasErrors()) {
-//            bindingResult.getAllErrors().forEach(objectError -> log.info(objectError.toString()));
-//        }
         gameService.changeDirection(gate);
         gameService.resetMessages();
         replace(webSession);
-//        log.info("process = {}",webSession.getId());
     }
 
     @PostMapping("/take")
@@ -139,47 +124,29 @@ public class GameRestController {
     @GetMapping("/restart")
     public void restart(WebSession webSession) {
         loadInitVariables(webSession);
-//        log.info("reset() called for = {}", webSession.getId());
+        log.info("reset() called for = {}", webSession.getId());
         gameService.reset();
         replace(webSession);
     }
 
     @GetMapping("/start")
-    public void home(WebSession webSession) {
+    public void start(WebSession webSession) {
         loadInitVariables(webSession);
-//        log.info("home() called for = {}", webSession.getId());
+        log.info("start() called for = {}", webSession.getId());
         if (gameService.isGameOver()) {
-//            log.info("reset() called for = {}", webSession.getId());
+            log.info("reset() called for = {}", webSession.getId());
             gameService.reset();
             replace(webSession);
         }
     }
 
     @GetMapping("/exit")
-    public void exit(Model model,WebSession webSession) {
+    public void exit(WebSession webSession) {
         loadInitVariables(webSession);
-//        log.info("exit() called for = {}",webSession.getId());
+        log.info("exit() called for = {}", webSession.getId());
         gameService.exit();
         replace(webSession);
     }
-
-//    @ModelAttribute("nextGate")
-//    public Gate nextGateInstance() {
-//        Gate nextGate = new Gate();
-//        return nextGate;
-//    }
-//
-//    @ModelAttribute("invItem")
-//    public Item invItemInstance() {
-//        Item invItem = new Item();
-//        return invItem;
-//    }
-//
-//    @ModelAttribute("dropItem")
-//    public Item dropItemInstance() {
-//        Item dropItem = new Item();
-//        return dropItem;
-//    }
 
 //    @ResponseStatus(HttpStatus.NOT_FOUND)
 //    @ExceptionHandler(NotFoundException.class)
